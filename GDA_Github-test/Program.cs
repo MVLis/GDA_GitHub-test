457c4817457a99d02af64858487bed8369be5b72
Marina Lis <94225938+MVLis@users.noreply.github.com>
2023-04-12 20:33:33 +0300

    feat: add sum of numbers

--- GDA_Github-test/Program.cs
+++ GDA_Github-test/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿public class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Введите первое число:");
+        double firstNumber = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine("Введите второе число:");
+        double secondNumber = Convert.ToDouble(Console.ReadLine());
+
+        double sum = firstNumber + secondNumber;
+        Console.WriteLine("Результат сложения " + sum);
+    }
+}
